03676dc6cfcb1bd6e8e08f79623ab6506c9d5949
NievinEvgeny <slip@Oldlord.localdomain>
2022-05-19 20:00:33 +0700

    Remove useless check

--- src/sbt/rpn.c
+++ src/sbt/rpn.c
@@ -33,8 +33,7 @@ int check_letters(char* str)
     int flag = 0;
     for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++)
     {
-        if (!(str[i] >= 'A' && str[i] <= 'Z') && str[i] != '+' && str[i] != '-' && str[i] != '*' && str[i] != '/' && str[i] != ')' && str[i] != '('
-            && str[i] != '\0' && str[i] != '\n')
+        if (!(str[i] >= 'A' && str[i] <= 'Z') && str[i] != '+' && str[i] != '-' && str[i] != '*' && str[i] != '/' && str[i] != ')' && str[i] != '(')
         {
             return -1;
         }
